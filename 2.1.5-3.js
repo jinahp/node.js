3156a7f4b0c3877d9c2d64bbf3b7250d785a9887
jinah <monsatfri@gmail.com>
2022-10-25 19:28:38 +0900

    no message

--- 2.1.5-3.js
+++
@@ -1,5 +0,0 @@
-var array = ['nodejs', {}, 10, true];
-var node = array[0];
-var obj = array[1];
-var boo1 = array[3];
-console.log(node, obj, boo1);
--- 2.1.5-4.js
+++
@@ -1,3 +0,0 @@
-const array = ['nodejs', {}, 10, true];
-const [node, obj, , bool] = array;
-console.log({ node, obj, bool });
